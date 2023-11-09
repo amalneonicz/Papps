1cfe80eafa6751ab3020f400f6aafdd310de24cf
amalneonicz <amal@neonicz.com>
2023-11-10 01:12:59 +0530

    first commit

---
+++ P-N-P/src/main/java/com/example/PNP/PNPApplication.java
@@ -0,0 +1,13 @@
+package com.example.PNP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PNPApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PNPApplication.class, args);
+	}
+
+}
---
+++ P-N-P/src/test/java/com/example/PNP/PNPApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.PNP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PNPApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
